c955f891c69ed0f7c5eee8881666a98e99f8557d
sergiotorresprimero <sergiotorresprimero@gmail.com>
2026-07-22 02:41:42 -0500

    se quito la seccion de reuniones y se coloco esta informacion en el footer

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -15,7 +15,7 @@
     },
     { 
       texto: 'Nuestras Reuniones', 
-      ruta: '/reuniones',
+      ruta: '#reuniones',
       desc: 'Conoce nuestros horarios',
       icono: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z'
     },
